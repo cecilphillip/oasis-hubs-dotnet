c5127807689ba51612ae3e26cae20afb497531e2
cecilphillip <cecilphillip@gmail.com>
2025-05-20 21:53:54 -0400

    fix double build on serviceProvider

--- src/OasisHubs.Site/Extensions.cs
+++ src/OasisHubs.Site/Extensions.cs
@@ -3,13 +3,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
 using OasisHubs.DbModels;
 using OasisHubs.Defaults.Extensions;
-using OasisHubs.Defaults.Extensions.Messaging;
 using OasisHubs.Site.Policies;
+using OasisHubs.Site.Workers;
 using Paramore.Brighter;
 using Paramore.Brighter.Extensions.DependencyInjection;
 using Paramore.Brighter.MessagingGateway.RMQ;
-using Paramore.Brighter.ServiceActivator.Extensions.DependencyInjection;
-using Paramore.Brighter.ServiceActivator.Extensions.Hosting;
 using RabbitMQ.Client;
 using ZiggyCreatures.Caching.Fusion;
 using Channel = System.Threading.Channels.Channel;
@@ -17,15 +15,13 @@ using Channel = System.Threading.Channels.Channel;
 namespace OasisHubs.Site;
 
 internal static class Extensions {
-   public static WebApplication ConfigureAppServices(this WebApplicationBuilder builder) {
+   public static void ConfigureAppServices(this WebApplicationBuilder builder) {
       
       builder.AddSqlServerDbContext<OasisHubsDbContext>("OasisHubsDb");
       builder.Services.AddCoreServices(builder.Configuration)
          .AddMessagingServices(builder.Configuration)
          .AddAuthServices()
          .AddRazorAppServices();
-      
-      return builder.Build();
    }
 
    public static WebApplication ConfigurePipeline(this WebApplication app) {
@@ -64,6 +60,7 @@ internal static class Extensions {
       });
 
       services.AddStripe();
+      services.AddHostedService<HubUsageWorker>();
       
       var redisConnection = configuration.GetConnectionString("redisCache");
       services.AddFusionCache()
@@ -88,97 +85,41 @@ internal static class Extensions {
 
    private static IServiceCollection AddMessagingServices(this IServiceCollection services,
       IConfiguration configuration) {
-      var rabbitConnectionString = configuration.GetConnectionString("redisCache");
+      
+      var rabbitConnectionString = configuration.GetConnectionString("rabbitServer");
       if (rabbitConnectionString is null)
          throw new Exception("RabbitMQ Connection information missing");
 
       var rmqMessagingGatewayConnection = new RmqMessagingGatewayConnection {
          Name = "OasisHubsRMQConnection",
-         AmpqUri = new AmqpUriSpecification(new Uri(rabbitConnectionString),
-            connectionRetryCount: 5, retryWaitInMilliseconds: 250),
+         AmpqUri = new AmqpUriSpecification(new Uri(rabbitConnectionString)),
          //https://www.rabbitmq.com/tutorials/amqp-concepts.html#exchange-direct
          Exchange = new Exchange(MessagingConstants.DEFAULT_EXCHANGE, ExchangeType.Direct, true),
          DeadLetterExchange =
-            new Exchange(MessagingConstants.DEFAULT_DLQ_EXCHANGE, ExchangeType.Fanout, true),
-         Heartbeat = 15,
+            new Exchange(MessagingConstants.DEFAULT_DLQ_EXCHANGE, ExchangeType.Direct, true),
          PersistMessages = true
       };
 
       // Configure command processor
-      services.AddBrighter(options => {
-            options.HandlerLifetime = ServiceLifetime.Scoped;
-            options.CommandProcessorLifetime = ServiceLifetime.Scoped;
-            options.MapperLifetime = ServiceLifetime.Singleton;
-         })
+      services.AddBrighter()
+         .UseInMemoryOutbox()
          .UseExternalBus(new RmqProducerRegistryFactory(
             rmqMessagingGatewayConnection,
-            new[] {
+            [
                new RmqPublication {
                   Topic = new RoutingKey(MessagingConstants.HOST_UPDATED_TOPIC),
-                  MaxOutStandingMessages = 20,
-                  WaitForConfirmsTimeOutInMilliseconds = 10000,
                   MakeChannels = OnMissingChannel.Create
                },
                new RmqPublication {
                   Topic = new RoutingKey(MessagingConstants.SUBSCRIPTION_ACTIVATED_TOPIC),
-                  MaxOutStandingMessages = 20,
-                  WaitForConfirmsTimeOutInMilliseconds = 10000,
                   MakeChannels = OnMissingChannel.Create
                },
                new RmqPublication {
                   Topic = new RoutingKey(MessagingConstants.FUNDS_TRANSFER_TOPIC),
-                  MaxOutStandingMessages = 20,
-                  WaitForConfirmsTimeOutInMilliseconds = 10000,
                   MakeChannels = OnMissingChannel.Create
                }
-            }).Create())
+            ]).Create())
          .AutoFromAssemblies();
-
-      // Configure activator
-      var subscriptions = new Paramore.Brighter.Subscription[] {
-         new RmqSubscription<ActivateHostAccountCommand>(
-            new SubscriptionName(MessagingConstants.HOST_UPDATE_SUBSCRIPTION),
-            new ChannelName(MessagingConstants.HOST_UPDATE_CHANNEL),
-            new RoutingKey(MessagingConstants.HOST_UPDATED_TOPIC),
-            deadLetterChannelName: new ChannelName(MessagingConstants.DEFAULT_DLQ_CHANNEL),
-            deadLetterRoutingKey: MessagingConstants.DEFAULT_DLQ_ROUTING_KEY,
-            requeueCount: 5,
-            runAsync: true, isDurable: true,
-            makeChannels: OnMissingChannel.Create),
-         new RmqSubscription<ActivateCustomerSubscriptionCommand>(
-            new SubscriptionName(MessagingConstants.SUBSCRIPTION_ACTIVATED_SUBSCRIPTION),
-            new ChannelName(MessagingConstants.SUBSCRIPTION_ACTIVATED_CHANNEL),
-            new RoutingKey(MessagingConstants.SUBSCRIPTION_ACTIVATED_TOPIC),
-            deadLetterChannelName: new ChannelName(MessagingConstants.DEFAULT_DLQ_CHANNEL),
-            deadLetterRoutingKey: MessagingConstants.DEFAULT_DLQ_ROUTING_KEY,
-            requeueCount: 5,
-            runAsync: true, isDurable: true,
-            makeChannels: OnMissingChannel.Create),
-         new RmqSubscription<InitiateFundsTransferCommand>(
-            new SubscriptionName(MessagingConstants.FUNDS_TRANSFER_SUBSCRIPTION),
-            new ChannelName(MessagingConstants.FUNDS_TRANSFER_CHANNEL),
-            new RoutingKey(MessagingConstants.FUNDS_TRANSFER_TOPIC),
-            deadLetterChannelName: new ChannelName(MessagingConstants.DEFAULT_DLQ_CHANNEL),
-            deadLetterRoutingKey: MessagingConstants.DEFAULT_DLQ_ROUTING_KEY,
-            requeueCount: 5,
-            runAsync: true, isDurable: true,
-            makeChannels: OnMissingChannel.Create)
-      };
-      services.AddServiceActivator(options => {
-            var rmqMessageConsumerFactory =
-               new RmqMessageConsumerFactory(rmqMessagingGatewayConnection);
-
-            options.UseScoped = true;
-            options.HandlerLifetime = ServiceLifetime.Scoped;
-            options.MapperLifetime = ServiceLifetime.Singleton;
-            options.CommandProcessorLifetime = ServiceLifetime.Scoped;
-            options.ChannelFactory = new ChannelFactory(rmqMessageConsumerFactory);
-            options.Subscriptions = subscriptions;
-         })
-         .UseInMemoryInbox()
-         .AutoFromAssemblies();
-
-      services.AddHostedService<ServiceActivatorHostedService>();
 
       return services;
    }
